952eaf16403da11d9814c2b325ace8ec2a5ef430
airtoxin <airtoxin@icloud.com>
2016-12-29 02:32:48 +0900

    Write stories

--- stories/index.js
+++ stories/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
+import { storiesOf, action } from '@kadira/storybook';
+import { PointyToppedHex, FlatToppedHex } from '../src/index.jsx';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
+storiesOf('PointyToppedHex', module)
+  .addWithInfo('simple usage', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <PointyToppedHex x={250} y={250} size={50}/>
+    </svg>
+  ), { inline: true, propTables: [PointyToppedHex] })
+  .addWithInfo('place in corner', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <PointyToppedHex x={0} y={0} size={50}/>
+    </svg>
+  ), { inline: true, propTables: [PointyToppedHex] })
+  .addWithInfo('big size', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <PointyToppedHex x={250} y={250} size={250}/>
+    </svg>
+  ), { inline: true, propTables: [PointyToppedHex] })
+  .addWithInfo('pass through props', '', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <PointyToppedHex
+        x={250}
+        y={250}
+        size={50}
+        fill="white"
+        stroke="black"
+        strokeWidth="10"
+        transform="rotate(15,250,250)"
+        onClick={action('onClick')}
+        onMouseEnter={action('onMouseEnter')}
+      />
+    </svg>
+  ), { inline: true, propTables: [PointyToppedHex] });
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ));
+storiesOf('FlatToppedHex', module)
+  .addWithInfo('simple usage', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <FlatToppedHex x={250} y={250} size={50}/>
+    </svg>
+  ), { inline: true, propTables: [FlatToppedHex] })
+  .addWithInfo('place in corner', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <FlatToppedHex x={0} y={0} size={50}/>
+    </svg>
+  ), { inline: true, propTables: [FlatToppedHex] })
+  .addWithInfo('big size', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <FlatToppedHex x={250} y={250} size={250}/>
+    </svg>
+  ), { inline: true, propTables: [FlatToppedHex] })
+  .addWithInfo('pass through props', '', () => (
+    <svg width="500" height="500" style={{border: '1px solid'}}>
+      <FlatToppedHex
+        x={250}
+        y={250}
+        size={50}
+        fill="white"
+        stroke="black"
+        strokeWidth="10"
+        transform="rotate(15,250,250)"
+        onClick={action('onClick')}
+        onMouseEnter={action('onMouseEnter')}
+      />
+    </svg>
+  ), { inline: true, propTables: [FlatToppedHex] });
