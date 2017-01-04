cba3204f9b3807036b599b60450f6e878936331a
airtoxin <airtoxin@icloud.com>
2017-01-04 16:48:17 +0900

    Add github corner

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import { configure, setAddon } from '@kadira/storybook';
+import { configure, addDecorator, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import GithubCorner from '@personare/react-storybook-decorator-github-corner';
+
+addDecorator(GithubCorner);
 
 setAddon(infoAddon);
 
